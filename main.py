0213c9980394de16d26c47428a602da95dcd20aa
dulledlove <108100952+dulledlove@users.noreply.github.com>
2022-06-24 03:30:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ setTimeout( (30) => {
     Channel.send(Picked)
         .then( msg => {
               @everyone rblxwild rain! join up.
-              <a href = "https://www.rblxwild.com"> 
+              https://www.rblxwild.com
             //If you wanna do anything like adding reactions
         }
 },1000);
