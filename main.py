09731338f553b929da575f0bb243ed703922c121
dulledlove <108100952+dulledlove@users.noreply.github.com>
2022-06-24 10:33:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-setTimeout( (5) <= {
+
+setTimeout( (5) = {
     let demo = ["a", "b", "c", "d"];
     let Picked = demo[Math.floor(Math.random() * demo.length)]);
     //I already explained the above part
     Channel.send(Picked)
-        .then( msg => {
+        .then( msg = {
             // rain notitfer message
               @everyone rblxwild rain! join up cuz it ends soon.  https://www.rblxwild.com   
             //If you wanna do anything like adding reactions
