dbbffda1a9296de050a284bf164cda01a8cc8c23
dulledlove <108100952+dulledlove@users.noreply.github.com>
2022-06-24 12:29:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ time.sleep(3)== {
         .then( msg = {
 print('rain embed configuring...')
 
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN})
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
 await lib.discord.channels['@0.3.0'].messages.create({
   "channel_id": `${context.params.event.channel_id}`,
