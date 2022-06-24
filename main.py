54dcc758fa923eb332be7aed53b6f86ccf85b0a0
dulledlove <108100952+dulledlove@users.noreply.github.com>
2022-06-24 03:31:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ setTimeout( (30) => {
             //If you wanna do anything like adding reactions
         }
 },1000);
+
+//rich presence code
+
+static void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "searching the world for rains...";
+    discordPresence.startTimestamp = 1507665886;
+    discordPresence.endTimestamp = 1507665886;
+    discordPresence.largeImageText = "Numbani";
+    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+    discordPresence.partyMax = 5;
+    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
+    Discord_UpdatePresence(&discordPresence);
+}
